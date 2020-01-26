6a51b07071dd96eeb985db550bec322c33f6dea2
Akshad2000 <54362624+Akshad2000@users.noreply.github.com>
2020-01-26 12:38:24 +0530

    Add files via upload
    how to take a user input from user

---
+++ scan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class addition
+{
+public static void main(String []ar)
+{
+
+        Scanner sc=new Scanner(System.in);
+        System.out,print("enter name");
+       String name=sc.next( );
+       System.out.print("Enter no1 & no2");
+       int x=sc.nextInt();
+       int y=sc.nextInt();
+        int z=x+y;
+       System.out.println("Name is"+name);
+       System.out.printf("the sum of %d&%d",x,y);
+}
+}     
+
